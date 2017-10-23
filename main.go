f44e0d4a9f5d382804048e944bbe5e44a7ad9460
shinytang6 <1074461480@qq.com>
2017-10-23 17:54:04 +0800

    add gitignore and delete img

--- main.go
+++ main.go
@@ -8,3 +8,18 @@ import (
 func main() {
 	beego.Run()
 }
+
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"os/exec"
+// 	// "github.com/fatih/color"
+// 	// "github.com/sajari/docconv"
+// 	"io"
+// 	// "log"
+// 	"os"
+// 	"strings"
+// )
